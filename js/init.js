46a980b35384ccc2950d88bc9c8ad11e86554515
Developer-felix <onjombafelix1@gmail.com>
2021-03-05 13:21:14 -0800

    my pizaa first beautiful ui

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.modal').modal();
+    $('.slider').slider();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
